5b0e1b9caa2666ac6867e236a43f2ff775405742
Pabllo Martins <nymbuslab@gmail.com>
2026-03-07 14:31:30 -0300

    feat: Replace investment calculation with age classification based on user input

---
+++ Exercicios-python/ex049.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+nome = input('Nome completo: ').strip() .title()
+ano_nascimento = int(input('Ano de nascimento: '))
+ano_atual = datetime.now().year
+idade = ano_atual - ano_nascimento
+
+if idade <= 12:
+    classificacao = 'Criança'
+elif idade <= 17:
+    classificacao = 'Adolescente'
+elif idade <= 59:
+    classificacao = 'Adulto'
+else:
+    classificacao = 'Idoso'
+
+print('\n')
+print(f'Nome: {nome}')
+print(f'Idade: {idade} anos')
+print('================')
+print(f'Nome: {nome}')
+print(f'Classificação: {classificacao}')
+print('================')
