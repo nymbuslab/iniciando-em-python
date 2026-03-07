5d3f878c2a9808dba23cc01108c8ea11a654d4d8
Pabllo Martins <nymbuslab@gmail.com>
2026-03-07 14:26:57 -0300

    feat: Replace investment calculation with age classification based on user input

--- ex049.py
+++ ex049.py
@@ -1,6 +1,23 @@
-valor_inicial = float(input('Digite o valor do investimento: R$'))
-taxa = int(input('Taxa de Juros (%): '))
-anos = int(input('Resgate em quantos anos: '))
-montante = valor_inicial * (1 + (taxa/100) * anos)
+from datetime import datetime
 
-print(montante)
+nome = input('Nome completo: ').strip() .title()
+ano_nascimento = int(input('Ano de nascimento: '))
+ano_atual = datetime.now().year
+idade = ano_atual - ano_nascimento
+
+if idade <= 12:
+    classificacao = 'Criança'
+elif idade <= 17:
+    classificacao = 'Adolescente'
+elif idade <= 59:
+    classificacao = 'Adulto'
+else:
+    classificacao = 'Idoso'
+
+print('\n')
+print(f'Nome: {nome}')
+print(f'Idade: {idade} anos')
+print('================')
+print(f'Nome: {nome}')
+print(f'Classificação: {classificacao}')
+print('================')
