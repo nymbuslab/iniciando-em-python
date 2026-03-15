f87a357b0616adac2bae23249a0bef6f15d31abe
Pabllo Martins <nymbuslab@gmail.com>
2026-03-15 07:31:41 -0300

    feat: Update color test script to include multiple color outputs

--- Exercicios-python/ex060.py
+++ Exercicios-python/ex060.py
@@ -2,6 +2,9 @@
 Teste de cores no terminal
 """
 
-s = 'prova de python'
-
-print(len(s))
+print('\033[0;30;41mTESTE\033[m')
+print('\033[4;33;44mTESTE\033[m')
+print('\033[1;35;43mTESTE\033[m')
+print('\033[30;42mTESTE\033[m')
+print('\033[mTESTE\033[m')
+print('\033[7;30mTESTE\033[m')
